18a18501855f983ade9c002327f504c4d68e4af6
drak3-4598 <51925580+drak3-4598@users.noreply.github.com>
2022-09-14 10:50:46 +0200

    finishi app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-const SearchBar = () => {
-    return <input/>;
-};
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+    }
+
+
+    render(){
+        return (
+            <div className='search-bar'>
+                <input 
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)}>
+                </input> 
+            </div>
+        );
+    }
+
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+
+   
+}
 
 export default SearchBar;
